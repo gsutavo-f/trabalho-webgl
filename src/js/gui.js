252cbf8a20c5a8fc1761ba2d670877534a8785d8
gsutavo-f <gustavoperesfernandes@gmail.com>
2021-10-01 03:10:06 -0300

    added animations

--- src/js/gui.js
+++ src/js/gui.js
@@ -26,6 +26,16 @@ let camera = {
   rotateCenter: false
 }
 
+let animations = {
+  playAnimation: () => {
+    requestAnimationFrame(animateObj)
+  },
+  whatAnimate: "Models",
+  animationType: "Rotation",
+  directionOption: "Positive",
+  animationValue: 0
+}
+
 const loadGUI = () => {
   const gui = new dat.GUI()
 
@@ -56,13 +66,21 @@ const loadGUI = () => {
 
   let cameraFolder = gui.addFolder("Camera")
   cameraFolder.open()
-
   cameraFolder.add(camera, "rotateCamera", -360, 360, 0.1).name("Rotate Camera")
   cameraFolder.add(camera, "cameraX", -150, 150, 0.1).name("Camera X")
   cameraFolder.add(camera, "cameraY", -150, 150, 0.1).name("Camera Y")
   cameraFolder.add(camera, "cameraZ",  1, 300, 0.1).name("Zoom")
   cameraFolder.add(camera, "lookCenter").name("Look at center")
   cameraFolder.add(camera, "rotateCenter").name("Rotate around center")
+
+  let animationsFolder = gui.addFolder("Animations")
+  animationsFolder.open()
+  animationsFolder.add(animations, "whatAnimate", ["Models", "Camera"]).name("What animate")
+  animationsFolder.add(animations, "animationType", ["Rotation", "Translation"]).name("Animation Type")
+  animationsFolder.add(animations, "directionOption", ["Positive", "Negative"]).name("Direction")
+  animationsFolder.add(animations, "animationValue", 0, 600, 10).name("Time")
+  animationsFolder.add(animations, "playAnimation").name("Play")
+
 }
 
 // functions using gui.js variables
@@ -95,4 +113,116 @@ function rotateCameraAroundCenter(matrix) {
   }
 
   return matrix
+}
+
+function animateObj(now) {
+  now *= 0.001
+  let then = now
+  let rotationTime = animations.animationValue
+  let translationTime = animations.animationValue
+  let animationSpeed = 10
+
+  if(animations.whatAnimate == "Models") {
+    if(animations.animationType == "Rotation") {
+      const animateRotation = (now) => {
+        now *= 0.001
+        let deltaTime = now - then
+        then = now
+    
+        if(animations.directionOption == "Positive") {
+          config.rotateX += deltaTime * animationSpeed;
+          if(config.rotateX < rotationTime) {
+            requestAnimationFrame(animateRotation)
+          } else {
+            config.rotateX = rotationTime
+          }
+        } else if(animations.directionOption == "Negative") {
+          config.rotateX -= deltaTime * animationSpeed
+          if(config.rotateX > (rotationTime * -1)) {
+            requestAnimationFrame(animateRotation)
+          } else {
+            config.rotateX = rotationTime
+          }
+        }
+      }
+      requestAnimationFrame(animateRotation)
+      config.rotateX = 0
+    } else if(animations.animationType == "Translation") {
+      const animateTranslation = (now) => {
+        now *= 0.001
+        let deltaTime = now - then
+        then = now
+    
+        if(animations.directionOption == "Positive") {
+          config.translateX += deltaTime * animationSpeed
+          if(config.translateX < translationTime) {
+            requestAnimationFrame(animateTranslation)
+          } else {
+            config.translateX = translationTime
+          }
+        } else if(animations.directionOption == "Negative") {
+          config.translateX -= deltaTime * animationSpeed
+          if(config.translateX > (translationTime * -1)) {
+            requestAnimationFrame(animateTranslation)
+          } else {
+            config.translateX = translationTime
+          }
+        }
+      }
+      requestAnimationFrame(animateTranslation)
+      config.translateX = 0
+    }
+  } else if(animations.whatAnimate == "Camera") {
+    if(animations.animationType == "Rotation") {
+      camera.rotateCenter = true
+      const animateCameraRotation = (now) => {
+        now *= 0.001
+        let deltaTime = now - then
+        then = now
+    
+        if(animations.directionOption == "Positive") {
+          camera.rotateCamera += deltaTime * animationSpeed;
+          if(camera.rotateCamera < rotationTime) {
+            requestAnimationFrame(animateCameraRotation)
+          } else {
+            camera.rotateCamera = rotationTime
+          }
+        } else if(animations.directionOption == "Negative") {
+          camera.rotateCamera -= deltaTime * animationSpeed
+          if(camera.rotateCamera > (rotationTime * -1)) {
+            requestAnimationFrame(animateCameraRotation)
+          } else {
+            camera.rotateCamera = rotationTime
+          }
+        }
+      }
+      requestAnimationFrame(animateCameraRotation)
+      camera.rotateCamera = 0
+    } else if(animations.animationType == "Translation") {
+      camera.rotateCenter = false
+      const animateCameraTranslation = (now) => {
+        now *= 0.001
+        let deltaTime = now - then
+        then = now
+    
+        if(animations.directionOption == "Positive") {
+          camera.cameraX += deltaTime * animationSpeed
+          if(camera.cameraX < translationTime) {
+            requestAnimationFrame(animateCameraTranslation)
+          } else {
+            camera.cameraX = translationTime
+          }
+        } else if(animations.directionOption == "Negative") {
+          camera.cameraX -= deltaTime * animationSpeed
+          if(camera.cameraX > (translationTime * -1)) {
+            requestAnimationFrame(animateCameraTranslation)
+          } else {
+            camera.cameraX = translationTime
+          }
+        }
+      }
+      requestAnimationFrame(animateCameraTranslation)
+      camera.cameraX = 0
+    }
+  }
 }
